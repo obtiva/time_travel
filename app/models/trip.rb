af30ebc5b72fc975c9116a265e7e9da79b5f5d9e
Noel Rappin <noelrappin@gmail.com>
2010-09-10 13:02:24 -0500

    now with working and all

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -5,9 +5,8 @@ class Trip < ActiveRecord::Base
   
   def preference_points(current_user)
     trip_points = 0
-    
+
     current_user.user_preferences.each do |pref|
-      p "#{pref.name} #{location}"
       if pref.kind == "century"
         if (start_date.to_s + "'s" == pref.name) 
           trip_points += 5
@@ -15,38 +14,45 @@ class Trip < ActiveRecord::Base
         if (end_date.to_s + "'s" == pref.name)
           trip_points += 5
         end
-      elsif (pref.name == "Other" && location == "Outer Space")
+      end
+      if (pref.name == "Other" && location == "Outer Space")
         trip_points += 3
-      elsif pref.kind == "area"
+      end
+      if pref.kind == "area"
         if (pref.name == location)
           trip_points += 7
         end
-      elsif (pref.name == "Europe" && location == "England")
+      end
+      if (pref.name == "Europe" && location == "England")
         trip_points += 5
-      elsif (pref.name == "USA" && location == "California")
-        p "here"
-        trip_points += 5
-      elsif (pref.name == "Mountain Climbing" && activity == "Hiking")
+      end
+      if (pref.name == "USA" && location == "California")
+        trip_points += 7
+      end
+      if (pref.name == "Mountain Climbing" && activity == "Hiking")
         trip_points += 3
-      elsif (pref.name == "Europe" && location == "France")
+      end
+      if (pref.name == "Europe" && location == "France")
         trip_points += 5
-      elsif pref.kind == "area"
-        if (pref.name == location)
-          trip_points += 7
-        end
-      elsif (pref.name == "Asia" && location == "China")
+      end
+      if (pref.name == "Asia" && location == "China")
         trip_points += 5
-      elsif (pref.name == "Concert Going" && activity == "Theater")
+      end
+      if (pref.name == "Concert Going" && activity == "Theater")
         trip_points += 3
-      elsif pref.kind == "activity"
+      end
+      if pref.kind == "activity"
         if (pref.name == activity)
           trip_points += 8
         end
-      elsif (pref.name == "Asia" && location == "Himalayas")
+      end
+      if (pref.name == "Asia" && location == "Himalayas")
         trip_points += 5
-      elsif (pref.name == "Europe" && location == "UK")
+      end
+      if (pref.name == "Europe" && location == "UK")
         trip_points += 3
-      elsif (pref.name == "Other" && location == "Atlantic Ocean")
+      end
+      if (pref.name == "Other" && location == "Atlantic Ocean")
         trip_points += 5
       end
     end
